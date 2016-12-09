23444d5c6efffaba113ce0da0e8a240fe5b221cb
Cory Capps <corycapps@Lalonde.local>
2016-12-08 23:45:57 -0600

    :gem: Added toggle feature to creating vertices.

--- client/controllers/graph.controller.js
+++ client/controllers/graph.controller.js
@@ -12,35 +12,48 @@
         
         var nextVertexId = 0;
         var vertexColor = "#CC8193";
+        vm.canDraw = false;
         
         // Set of vertices in graph - V(G)
         vm.vertices = [
             {
-                id: 0, // id of vertex
-                xPos: 0, // x-position
-                yPos: 0, // y-position
-                weight: 0, // vertex weight (if applicable)
-                size: 0, // size of vertex (radius)
-                text: '' // vertex name/weight
+                //id: 0, // id of vertex
+                //xPos: 0, // x-position
+                //yPos: 0, // y-position
+                //weight: 0, // vertex weight (if applicable)
+                //size: 0, // size of vertex (radius)
+                //text: '' // vertex name/weight
             },
         ];
         
         // Set of edges in graph - E(G)
         vm.edges = [
             {
-                
+                //v1: 0,
+                //v2: 0
+            },
+            {
+                v1: 1,
+                v2: 2
+            },
+            {
+                v1: 3,
+                v2: 1
             }
         ];
         
-        vm.nextChar = function(next){
-            if(next < 26) {
-                return String.fromCharCode('A'.charCodeAt(0) + next);    
+        // Function: nextChar(currentVertex)
+        // Increments through alphabet to add corresponding
+        // letter on newly inserted verticies
+        vm.nextChar = function(currentVertex){
+            if(currentVertex < 26) {
+                return String.fromCharCode('A'.charCodeAt(0) + currentVertex);    
             }
             else {
                 return "[ ]";
-            }
-            
+            } 
         }
+        
         // Function: addVertex(event)
         // adds vertex to V(G)
         vm.addVertex = function(event) {
@@ -58,6 +71,11 @@
             });
             nextVertexId++;
             
+            console.log("Vertex " + vm.nextChar(nextVertexId - 1) + ": ");
+            console.log("\txPos: " + vm.vertices[nextVertexId].xPos);
+            console.log("\tyPos: " + vm.vertices[nextVertexId].yPos);
+            
+            vm.canDraw = false;
             vm.update();
         }
         
